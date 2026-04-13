5ddbdc5f1a7f8890e47db3266227eda48ee4324c
naresh580 <nareshpandey580@gmail.com>
2026-04-13 22:47:22 +0530

    initialize authentication-back-end app

---
+++ src/main/java/com/personal/auth/authentication_back_end_app/AuthenticationBackEndAppApplication.java
@@ -0,0 +1,13 @@
+package com.personal.auth.authentication_back_end_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationBackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationBackEndAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/auth/authentication_back_end_app/AuthenticationBackEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.auth.authentication_back_end_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationBackEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
